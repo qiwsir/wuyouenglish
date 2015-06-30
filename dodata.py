5b2c339e688e252a1cd70a369c29ff867d124284
qiwsir <qiwsir@gmail.com>
2015-06-30 22:43:41 +0800

    new data

--- dodata.py
+++ dodata.py
@@ -3,12 +3,20 @@
 
 import csv
 
-def read_data(filename):
+def read_data(filename, start_id):
     csv_reader = csv.reader(open(filename))
+    new_data = {}
     for row in csv_reader:
-        print row
+        if row[3]:        
+            value_row = [cell for cell in row[1:]]
+            if start_id != row[0]:
+                start_id = row[0]
+
+            new_data[start_id] = []
+            new_data[start_id].append(value_row)
+    return new_data
 
 
 if __name__ == "__main__":
-    read_data("w.csv")
+    print read_data("w.csv", "00001")
 
